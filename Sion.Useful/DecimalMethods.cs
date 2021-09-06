acfe99e217243f030adea86dcd62df8ec0005cce
Sion Pixley <sionpixley@gmail.com>
2021-09-06 11:24:09 -0500

    Added Sion.Useful.DecimalMethods.Truncate().

---
+++ Sion.Useful/DecimalMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sion.Useful {
+	public static class DecimalMethods {
+		// Drops decimal points without rounding.
+		public static decimal Truncate(decimal d, int precision = 4) {
+			string strD = d.ToString();
+			int index = strD.IndexOf('.');
+
+			if(index == -1) {
+				return d;
+			}
+			else if((index + precision + 1) >= strD.Length) {
+				return d;
+			}
+			else {
+				return Convert.ToDecimal(strD[..(index + precision + 1)]);
+			}
+		}
+	}
+}
