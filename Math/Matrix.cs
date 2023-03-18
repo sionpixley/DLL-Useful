f7d304643722a1110cdb54f0f7a293f24a5a093a
Sion Pixley <sionpixley@gmail.com>
2023-03-17 23:05:22 -0500

    Changing the structure a bit more.

---
+++ Math/Matrix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sion.Useful.Math {
+	public class Matrix<T> where T : IComparable<T>, IEquatable<T> {
+		private T[,] _Matrix;
+
+		public Matrix() {
+			_Matrix = new T[1, 1];
+		}
+
+		public Matrix(int rows, int columns) {
+			_Matrix = new T[rows, columns];
+		}
+
+		public Matrix(Matrix<T> matrix) {
+
+		}
+	}
+}
