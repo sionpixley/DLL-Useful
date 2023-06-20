cd0294075d60c2209b4087ba5632c06fb7282579
Sion Pixley <sionpixley@gmail.com>
2023-06-19 19:33:11 -0500

    Adding some extra methods to the StringMethods class.

--- Sion.Useful/StringMethods.cs
+++ Sion.Useful/StringMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Sion.Useful {
@@ -6,6 +7,115 @@ namespace Sion.Useful {
 		public static string Base64ToUtf8(string base64) {
 			byte[] raw = Convert.FromBase64String(base64);
 			return Encoding.UTF8.GetString(raw);
+		}
+
+		public static string CamelCaseToPascalCase(string camelCase) {
+			return camelCase.Length > 0 ? $"{Char.ToUpper(camelCase[0])}{camelCase[1..]}" : "";
+		}
+
+		public static string CamelCaseToSnakeCase(string camelCase) {
+			List<string> builder = new();
+			List<char> chars = new();
+			foreach(var c in camelCase) {
+				if(Char.IsUpper(c)) {
+					builder.Add(String.Concat(chars));
+					chars.Clear();
+					chars.Add(Char.ToLower(c));
+				}
+				else {
+					chars.Add(c);
+				}
+			}
+			if(chars.Count > 0) {
+				builder.Add(String.Concat(chars));
+			}
+			return String.Join("_", builder);
+		}
+
+		public static string PascalCaseToCamelCase(string pascalCase) {
+			return pascalCase.Length > 0 ? $"{Char.ToLower(pascalCase[0])}{pascalCase[1..]}" : "";
+		}
+
+		public static string PascalCaseToSnakeCase(string pascalCase) {
+			List<string> builder = new();
+			List<char> chars = new();
+			bool firstChar = true;
+			foreach(var c in pascalCase) {
+				if(firstChar) {
+					chars.Add(Char.ToLower(c));
+					firstChar = false;
+				}
+				else if(Char.IsUpper(c)) {
+					builder.Add(String.Concat(chars));
+					chars.Clear();
+					chars.Add(Char.ToLower(c));
+				}
+				else {
+					chars.Add(c);
+				}
+			}
+			if(chars.Count > 0) {
+				builder.Add(String.Concat(chars));
+			}
+			return String.Join("_", builder);
+		}
+
+		public static string SnakeCaseToCamelCase(string snakeCase) {
+			List<string> builder = new();
+			List<char> chars = new();
+			bool firstChar = true;
+			bool newWord = false;
+			foreach(var c in snakeCase) {
+				if(c == '_') {
+					newWord = true;
+				}
+				else if(firstChar) {
+					chars.Add(Char.ToLower(c));
+					firstChar = false;
+				}
+				else if(newWord) {
+					builder.Add(String.Concat(chars));
+					chars.Clear();
+					chars.Add(Char.ToUpper(c));
+					newWord = false;
+				}
+				else {
+					chars.Add(Char.ToLower(c));
+				}
+			}
+			if(chars.Count > 0) {
+				builder.Add(String.Concat(chars));
+			}
+			return String.Concat(builder);
+		}
+
+		public static string SnakeCaseToPascalCase(string snakeCase) {
+			List<string> builder = new();
+			List<char> chars = new();
+			bool firstChar = true;
+			bool newWord = false;
+			foreach(var c in snakeCase) {
+				if(c == '_') {
+					newWord = true;
+				}
+				else if(firstChar) {
+					chars.Add(Char.ToUpper(c));
+					firstChar = false;
+				}
+				else if(newWord) {
+					builder.Add(String.Concat(chars));
+					chars.Clear();
+					chars.Add(Char.ToUpper(c));
+					newWord = false;
+				}
+				else {
+					chars.Add(Char.ToLower(c));
+				}
+			}
+			if(chars.Count > 0) {
+				builder.Add(String.Concat(chars));
+			}
+			return String.Concat(builder);
 		}
 
 		public static string Utf8ToBase64(string utf8) {
--- Tests/StringMethods.cs
+++ Tests/StringMethods.cs
@@ -20,6 +20,186 @@ namespace Tests {
 		}
 
 		[TestMethod]
+		public void CamelCaseToPascalCase() {
+			try {
+				string camelCase = "helloTesting";
+				string expected = "HelloTesting";
+
+				string result = Sion.Useful.StringMethods.CamelCaseToPascalCase(camelCase);
+
+				Assert.AreEqual(expected, result);
+			}
+			catch(Exception e) {
+				Assert.Fail(e.Message);
+			}
+		}
+
+		[TestMethod]
+		public void CamelCaseToPascalCase_Empty() {
+			try {
+				string camelCase = "";
+				string expected = "";
+
+				string result = Sion.Useful.StringMethods.CamelCaseToPascalCase(camelCase);
+
+				Assert.AreEqual(expected, result);
+			}
+			catch(Exception e) {
+				Assert.Fail(e.Message);
+			}
+		}
+
+		[TestMethod]
+		public void CamelCaseToSnakeCase() {
+			try {
+				string camelCase = "helloTesting";
+				string expected = "hello_testing";
+
+				string result = Sion.Useful.StringMethods.CamelCaseToSnakeCase(camelCase);
+
+				Assert.AreEqual(expected, result);
+			}
+			catch(Exception e) {
+				Assert.Fail(e.Message);
+			}
+		}
+
+		[TestMethod]
+		public void CamelCaseToSnakeCase_Empty() {
+			try {
+				string camelCase = "";
+				string expected = "";
+
+				string result = Sion.Useful.StringMethods.CamelCaseToSnakeCase(camelCase);
+
+				Assert.AreEqual(expected, result);
+			}
+			catch(Exception e) {
+				Assert.Fail(e.Message);
+			}
+		}
+
+		[TestMethod]
+		public void PascalCaseToCamelCase() {
+			try {
+				string pascalCase = "HelloTesting";
+				string expected = "helloTesting";
+
+				string result = Sion.Useful.StringMethods.PascalCaseToCamelCase(pascalCase);
+
+				Assert.AreEqual(expected, result);
+			}
+			catch(Exception e) {
+				Assert.Fail(e.Message);
+			}
+		}
+
+		[TestMethod]
+		public void PascalCaseToCamelCase_Empty() {
+			try {
+				string pascalCase = "";
+				string expected = "";
+
+				string result = Sion.Useful.StringMethods.PascalCaseToCamelCase(pascalCase);
+
+				Assert.AreEqual(expected, result);
+			}
+			catch(Exception e) {
+				Assert.Fail(e.Message);
+			}
+		}
+
+		[TestMethod]
+		public void PascalCaseToSnakeCase() {
+			try {
+				string pascalCase = "HelloTesting";
+				string expected = "hello_testing";
+
+				string result = Sion.Useful.StringMethods.PascalCaseToSnakeCase(pascalCase);
+
+				Assert.AreEqual(expected, result);
+			}
+			catch(Exception e) {
+				Assert.Fail(e.Message);
+			}
+		}
+
+		[TestMethod]
+		public void PascalCaseToSnakeCase_Empty() {
+			try {
+				string pascalCase = "";
+				string expected = "";
+
+				string result = Sion.Useful.StringMethods.PascalCaseToSnakeCase(pascalCase);
+
+				Assert.AreEqual(expected, result);
+			}
+			catch(Exception e) {
+				Assert.Fail(e.Message);
+			}
+		}
+
+		[TestMethod]
+		public void SnakeCaseToCamelCase() {
+			try {
+				string snakeCase = "hello_testing";
+				string expected = "helloTesting";
+
+				string result = Sion.Useful.StringMethods.SnakeCaseToCamelCase(snakeCase);
+
+				Assert.AreEqual(expected, result);
+			}
+			catch(Exception e) {
+				Assert.Fail(e.Message);
+			}
+		}
+
+		[TestMethod]
+		public void SnakeCaseToCamelCase_Empty() {
+			try {
+				string snakeCase = "";
+				string expected = "";
+
+				string result = Sion.Useful.StringMethods.SnakeCaseToCamelCase(snakeCase);
+
+				Assert.AreEqual(expected, result);
+			}
+			catch(Exception e) {
+				Assert.Fail(e.Message);
+			}
+		}
+
+		[TestMethod]
+		public void SnakeCaseToPascalCase() {
+			try {
+				string snakeCase = "hello_testing";
+				string expected = "HelloTesting";
+
+				string result = Sion.Useful.StringMethods.SnakeCaseToPascalCase(snakeCase);
+
+				Assert.AreEqual(expected, result);
+			}
+			catch(Exception e) {
+				Assert.Fail(e.Message);
+			}
+		}
+
+		[TestMethod]
+		public void SnakeCaseToPascalCase_Empty() {
+			try {
+				string snakeCase = "";
+				string expected = "";
+
+				string result = Sion.Useful.StringMethods.SnakeCaseToPascalCase(snakeCase);
+
+				Assert.AreEqual(expected, result);
+			}
+			catch(Exception e) {
+				Assert.Fail(e.Message);
+			}
+		}
+
+		[TestMethod]
 		public void Utf8ToBase64() {
 			try {
 				string expected = "8J+Ruw==";
