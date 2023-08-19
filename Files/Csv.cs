5170bd4e6c408066ca22b37ebf3b9a813b465b66
Sion Pixley <sionpixley@gmail.com>
2023-08-19 16:57:59 -0500

    Reference stuff

--- Files/Csv.cs
+++ Files/Csv.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Common;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Sion.Useful.Files {
 	public static class Csv {
