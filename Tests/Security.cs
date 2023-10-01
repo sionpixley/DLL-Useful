e2142a153d62a023170b1e11ff37ed6ba20fd5aa
Sion Pixley <sionpixley@gmail.com>
2023-10-01 11:57:34 -0500

    Starting to add some more unit tests. Pushing directly to main, and also not making a new release

---
+++ Tests/Security.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Random = Sion.Useful.Security.Random;
+
+namespace Tests {
+	[TestClass]
+	public class Security {
+		[TestMethod]
+		public void Random_Bool() {
+			try {
+				List<bool> trueList = new();
+				List<bool> falseList = new();
+				for(int i = 0; i < 1000000; i += 1) {
+					bool result = Random.Bool();
+					if(result) {
+						trueList.Add(true);
+					}
+					else {
+						falseList.Add(false);
+					}
+				}
+				double truePercent = trueList.Count / 1000000.0;
+				double falsePercent = falseList.Count / 1000000.0;
+				Assert.IsTrue(truePercent > 0.45 && falsePercent > 0.45);
+			}
+			catch(Exception e) {
+				Assert.Fail(e.Message);
+			}
+		}
+	}
+}
